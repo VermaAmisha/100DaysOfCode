44ae89ec67ad21a0ab308ae2c2701eb521f7b7bf
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-10-19 20:35:36 +0530

    A program to switch  the values of two variables.

---
+++ day1/day1-9variables.py
@@ -0,0 +1,13 @@
+# Don't change the code below
+a = input("a: ")
+b = input("b: ")
+# Don't change the code above
+
+c = a
+a = b
+b = c
+
+# Don't change the code below
+print("a = " + a)
+print("b = " + b)
+# Don't change the code above

d3d7babc90b823c7b21af82b8b451c909265396d
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-10-19 20:38:52 +0530

    A project program for the end of Day 1.
    It is a summation of different thing learnt in Day 1 of 100 Days Of Coding.

---
+++ day1/day1-project.py
@@ -0,0 +1,13 @@
+#1 Create a greeting for your program
+print("Welcome to shop name generator!")
+
+#2 Ask the user for the city he/she grew up in
+city= input("Which city did you grow up in?\n")
+
+#3 Ask the user for the name of his/her pet
+pet= input("What is the name of your pet?\n")
+
+#4 combine the name of his/her city and pet and show them his/her band name
+print("Your shop name could be",city,pet)
+
+#5 Make sure that the input cursor shows on the new line

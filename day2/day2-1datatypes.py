4044250cb048f5b685747b5b1ee1c17dc6f1d093
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-10-20 21:36:46 +0530

    A program including four primitive data types.

---
+++ day2/day2-1datatypes.py
@@ -0,0 +1,22 @@
+# String
+# to print the 1st element of "Hello"
+print("Hello"[0])
+# to print the 5th element of "Hello"
+print("Hello"[4])
+
+# nos. inside quotation marks are considered as strings
+print("123" + "345")
+
+# Integer
+print(123 + 345)
+# large numbers are written using underscores
+print(123_456_789)
+
+# Float
+# the decimal point may float to different places
+print(3.1435 , 314.35)
+
+# Boolean
+# it tells if the statement is True or False
+x = 5
+print(x > 2)

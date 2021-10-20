0400adaebcdac0767e781f741c925018602035ec
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-10-20 23:28:19 +0530

    A program using f-String.

---
+++ day2/day2-9f-string.py
@@ -0,0 +1,6 @@
+score = 0
+height = 1.8
+isWinning = True
+# f-String is used to merge different data types 
+print(f"Your score is {score}")
+print(f"Your height is {height}, You're winning is {isWinning}")

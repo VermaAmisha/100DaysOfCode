b8620d71f06563f37891c3565ee037a82cb1fdd4
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-10-20 21:50:57 +0530

    A program to solve a mathematical question.

---
+++ day2/day2-5simplify.py
@@ -0,0 +1,11 @@
+# PEMDAS - Parentheses() Exponents** Multiplication* Division/ Addition+ Subtraction-
+# In Python - P E M/D A/S - M and D has same importance , A and S has same importance
+# simplification priorities goes from Left to Right 
+
+print(3 * 3 + 3 / 3 - 3)
+
+# here 1st * , 2nd / , 3rd + , 4th -
+
+print(3 / 3 - 3 * 3 + 3)
+
+# here 1st / , 2nd * , 3rd - , 4th +

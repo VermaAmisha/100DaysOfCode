146736198ea07a9b2f98a9372d6ea1da107efa4c
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-10-20 21:41:17 +0530

    A program to check the types of  data used.

---
+++ day2/day2-2typecheck.py
@@ -0,0 +1,12 @@
+# Data type of any code can be checked by using type() function 
+
+x = 12335
+print(type(x))
+
+x = "12345"
+print(type(x))
+x = "abcd"
+print(type(x))
+
+x = 567.89
+print(type(x))

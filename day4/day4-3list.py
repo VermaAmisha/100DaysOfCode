a5d9be38c033fe19ce37e490d019b5cf4d64ff5b
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-10-22 23:27:03 +0530

    A program using lists in Python.

---
+++ day4/day4-3list.py
@@ -0,0 +1,18 @@
+letters = ["a", "b", "c", "d", "e"]
+
+print(letters[0])
+
+# To print letters from behind negative index is used
+
+print(letters[-3])
+
+#  To make changes in or replace items in the list
+
+letters[1] = "s"
+print(letters)
+
+# To add an item to the list - it is usually added to the end of the list
+
+letters.append("f")
+print(letters)
+

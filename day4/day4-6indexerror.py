53ae49615449bc9c34a6856d8d748609babd7dd9
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-10-22 23:52:19 +0530

    A program to understand index error

---
+++ day4/day4-6indexerror.py
@@ -0,0 +1,7 @@
+letters = ["a", "b","c","d","e"]
+
+length = len(letters)
+
+print(letters[length - 1])
+
+#  -1 is important as index count starts from 0 so the last item will not be considered as 5 but as 4

45423c4e67a9cb5fd87bc59974f3248ff36d24c2
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-10-23 23:17:11 +0530

    A program to add even numbers using range()

---
+++ day5/day5-6addevennumbers.py
@@ -0,0 +1,12 @@
+total = 0
+for i in range(2 , 101 , 2):
+    total += i
+print(total)    # To find the total of all the even numbers all together
+
+print("************")
+
+total = 0
+for i in range(2 , 101 , 2):
+    total += i
+    print(total)    # To find the total of all the even numbers in every step
+

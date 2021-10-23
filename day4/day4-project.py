9bdb13581a8bc9f7d3f0fcf26bdaabb60370031b
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-10-23 14:48:18 +0530

    Rock Paper Scissors game in Python.

---
+++ day4/day4-project.py
@@ -0,0 +1,45 @@
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+import random
+
+user_choice = int(input("Which number do you want to choose? Type 0 for Rock, 1 for Paper , 2 for Scissors\n"))
+
+computer_choice = random.randint(0,2)
+
+print(computer_choice)
+
+if computer_choice == user_choice:
+    print("It is a tie!")
+elif computer_choice == 1 and user_choice == 0:
+    print("Computer wins!")
+elif computer_choice == 2 and user_choice == 1:
+    print("Computer wins!")
+elif computer_choice == 0 and user_choice == 2:
+    print("Computer wins!")
+else:
+    print("User wins!")

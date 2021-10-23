4ee1f83aafbc3bd61c20708ddb9a4979aa367a48
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-10-23 20:12:08 +0530

    A program using For Loop.

---
+++ day5/day5-1forloop.py
@@ -0,0 +1,32 @@
+fruits = ["Apple", "Berry", "Cherry"]
+for i in fruits:
+    print(i)
+
+# print("**************") is written for as to separate the codes from one another 
+print("**************")
+# Here print(i + " pie") is indented so as to include it inside for loop
+
+fruits = ["Apple", "Berry", "Cherry"]
+for i in fruits:
+    print(i)
+    print(i + " pie")
+
+print("**************")
+
+fruits = ["Apple", "Berry", "Cherry"]
+for i in fruits:
+    print(i)
+    print(i + " pie")
+    print(fruits)
+
+print("**************")
+
+# Here print(fruits) is written without giving indentation which makes it move out of for loop
+# If there is no indentation before print() function then it will come out of for loop
+
+fruits = ["Apple", "Berry", "Cherry"]
+for i in fruits:
+    print(i)
+    print(i + " pie")
+print(fruits)
+

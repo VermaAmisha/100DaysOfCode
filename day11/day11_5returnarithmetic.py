33a1946c9b390d92d9bed97392b180d93bd0337d
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-11-06 21:06:51 +0530

    A program using return to get arithmetic values.

---
+++ day11/day11_5returnarithmetic.py
@@ -0,0 +1,31 @@
+
+first_number = int(input("Enter the value of n1: "))
+second_number = int(input("Enter the value of n2: "))
+
+# add
+
+def add(n1 , n2):
+    return n1 + n2
+
+print(add(first_number , second_number))
+
+# subtract
+
+def subtract(n1 , n2):
+    return n1 - n2
+
+print(subtract(first_number , second_number))
+
+# multiply
+
+def multiply(n1 , n2):
+    return n1 * n2
+
+print(multiply(first_number , second_number))
+
+# divide
+
+def divide(n1 , n2):
+    return n1 / n2
+
+print(divide(first_number , second_number))

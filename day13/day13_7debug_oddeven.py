d62606f430266f90aab6ff58d1f1ce7430889157
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-11-10 23:30:07 +0530

    A program to debug.

---
+++ day13/day13_7debug_oddeven.py
@@ -0,0 +1,18 @@
+# number = int(input("Which number do you want to check?"))
+
+# if number % 2 = 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+  
+
+
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
+# if number % 2= 0: -- this is syntax error , it should use == 
+# as == is used to check the answer and = is used to assign something

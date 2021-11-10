7c710520481e6b846b4105c948004f52a1c2feb5
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-11-10 21:09:30 +0530

    A program to be debugged by using print statement.

---
+++ day13/day13_5print.py
@@ -0,0 +1,18 @@
+ #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))   # this variable consists == which assigns its value to 0
+# total_words = pages * word_per_page
+# print(total_words)
+
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+# to get to know the problamatic area print all the variable values
+print(f"pages = {pages} pages")
+print(f"word_per_page = {word_per_page} words")
+
+print(total_words)

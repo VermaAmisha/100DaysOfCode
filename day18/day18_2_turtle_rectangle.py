0fbf1be42db0d69183bbab2aa9cc063508725987
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-11-17 23:34:06 +0530

    A program to make a rectangle using turtle.

---
+++ day18/day18_2_turtle_rectangle.py
@@ -0,0 +1,24 @@
+
+from turtle import Turtle , Screen
+
+timmy = Turtle()
+
+timmy.shape("turtle")
+timmy.color("DarkMagenta" , "DarkGreen")
+
+# rectangle shape by turtle
+
+timmy.back(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(200)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+
+my_screen = Screen()
+
+my_screen.exitonclick()

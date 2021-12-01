bf79e5dead1522860b74aceaff29a157ad771dfe
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-12-01 23:16:48 +0530

    A program to change the attribute of the table created

---
+++ day16/day16_10_change_attribute.py
@@ -0,0 +1,12 @@
+
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("S.no.",[1,2,3,4,5])
+table.add_column("Pokemon Name", ['Squirtle', 'Pikachu', 'Whismur', 'Slurpuff', 'Mothy'])  
+table.add_column("Type", ['Water', 'Electric','Normal','Fairy', 'Flying'])
+table.align = 'l'
+
+print(table)
+

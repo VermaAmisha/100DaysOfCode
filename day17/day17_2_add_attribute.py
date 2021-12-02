fbb9657b66e5582f965ac022dc86ec1cfb5ea8b9
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-12-02 23:58:29 +0530

    A program to add an attribute to the class

---
+++ day17/day17_2_add_attribute.py
@@ -0,0 +1,11 @@
+
+class user:    # to create a class
+    pass 
+# if you want to keep your class empty then write 'pass' with an indentation
+
+user_1 = user()
+
+user_1.id = "001"   # to create attribue, we can create as many attributes as we want
+user_1.username = "Amisha"
+
+print(user_1.username )

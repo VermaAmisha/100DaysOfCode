49a6bfb306f88891fc0cc1b6a4ee304b5bf61b00
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2021-12-24 23:45:43 +0530

    A program to create new screen for the pong game

---
+++ day22/day22_1_main_screen.py
@@ -0,0 +1,17 @@
+
+# 1. Score -- change both sides
+# 2. balls -- bounce
+# 3. boards -- 2 animated board witth keys to move
+# 4. screen -- with a mid division
+
+from turtle import Turtle,Screen, width
+
+# to setup screen black in color
+
+my_screen = Screen()
+my_screen.bgcolor("black")
+my_screen.setup(width= 800 , height= 600)
+my_screen.title("The Pong Game")
+my_screen.exitonclick()
+
+

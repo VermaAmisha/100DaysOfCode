7d905f65753dffcb744874eaefb5e603643179af
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-01-06 23:13:57 +0530

    A GUI program

---
+++ day27/day27_2_function_header.py
@@ -0,0 +1,19 @@
+
+import tkinter
+
+window = tkinter.Tk()
+
+window.title("My First GUI program")
+window.minsize(width=500, height=300)
+
+# label
+
+my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24,"bold"))
+my_label.pack()  # the label is at top center until and unless this packer label line of code is not written the albel will not be seen on the window
+
+# my_label.pack(side="left") - the label is moved to left similarly the label can be moved to top,bottom and right
+# my_label.pack(expand=True) - in the middle of the window
+
+
+
+window.mainloop()

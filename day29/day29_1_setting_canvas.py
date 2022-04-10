5681057729673a8c6e2cd24e06321e73138ec673
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-04-10 21:42:24 +0530

    A program to set canvas for the password generator program.

---
+++ day29/day29_1_setting_canvas.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+# Create window
+
+window = Tk()
+window.title("Password Generator")
+window.config(padx = 20 , pady = 20 , bg = "white")
+
+# Create canvas
+canvas = Canvas(width = 200, height= 200 , bg = "white" , highlightthickness= 0)
+canvas.pack()
+
+
+window.mainloop()

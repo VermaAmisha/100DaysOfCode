b2341d8d1209a2ba12b1391615578b70188206a3
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-05-17 23:57:35 +0530

    To set up a better User Interface of the flashcard game.

--- day31/day31_1_flash_card_UI.py
+++ day31/day31_1_flash_card_UI.py
@@ -8,16 +8,29 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
 window.title("Flash Card")
-window.config(padx = 50 , pady = 50)
+window.config(padx = 50 , pady = 50 , bg = BACKGROUND_COLOR)
 
-right_img = PhotoImage(file = "path/to/right.png")
-right_button = Button(image = right_img, highlightthickness = 0)
 
-wrong_img = PhotoImage(file = "path/to/wrong.png")
-button = Button(image= wrong_img, highlightthickness = 0)
+canvas = Canvas(width = 800, height= 526 , bg = "white" , highlightthickness= 0)
+card_front_img = PhotoImage(file = "day31/card_front.png")
 
-canvas = Canvas(width = 200, height= 200 , bg = "white" , highlightthickness= 0)
-canvas.grid(row = 0 , column = 1)
+canvas.grid(row = 0 , column = 0, columnspan = 2)
+canvas.config(bg= BACKGROUND_COLOR)
+
+canvas.create_image(400 , 263 , image = card_front_img)
+
+french_title = canvas.create_text(400 , 150 , text = "French" , font = ("Arial" , 30 , "bold"))
+french_word = canvas.create_text(400 , 263, text = "word", font = ("Arial" , 60 , "bold"))
+
+wrong_image = PhotoImage(file = "day31/wrong.png")
+wrong_button = Button(image = wrong_image , highlightthickness= 0)
+wrong_button.grid(row = 1 , column = 0)
+
+right_image = PhotoImage(file = "day31/right.png")
+right_button = Button(image = right_image , highlightthickness= 0)
+right_button.grid(row = 1 , column = 1)
+
+
 
 
 window.mainloop()

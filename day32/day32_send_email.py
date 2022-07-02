e20bfdbbdc8f6ce84a14a503ca9476006259fac7
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-07-02 23:31:59 +0530

    To avoid closing the connection separately.

--- day32/day32_send_email.py
+++ day32/day32_send_email.py
@@ -3,12 +3,28 @@ import smtplib
 my_email = "amishaverma.2612@gmail.com"
 password = "Amisha@123"
 
-connection = smtplib.SMTP("smtp.gmail.com")
+# connection = smtplib.SMTP("smtp.gmail.com")
 
-# "starttls" makes the connection secure tls is 'transport layer security'
-connection.starttls()
+# # "starttls" makes the connection secure tls is 'transport layer security'
+# connection.starttls()
 
-connection.login(user=my_email , password=password)
+# connection.login(user=my_email , password=password)
 
-connection.sendmail(from_addr=my_email , to_addrs="ankitaverma1501@gmail.com" ,msg="Hello")
-connection.close()
+# connection.sendmail(from_addr=my_email , 
+# to_addrs="ankitaverma1501@gmail.com" ,
+# msg="Subject:Hello\n\nThis the mail I am sending you via Python code!!!")
+# connection.close()
+
+# # # # # To avoid closing the connection separately # # # # #
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+
+    # "starttls" makes the connection secure tls is 'transport layer security'
+    connection.starttls()
+
+    connection.login(user=my_email , password=password)
+
+    connection.sendmail(from_addr=my_email , 
+    to_addrs="ankitaverma1501@gmail.com" ,
+    msg="Subject:Hello\n\nThis the mail I am sending you via Python code!!!")
+    
